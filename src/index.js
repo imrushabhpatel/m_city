9b990616c6483111737ddb9908a0aeab6c51084e
Rushabh Patel <irushabhpatel@gmail.com>
2021-01-14 17:23:59 -0500

    14_Jan

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './Resources/css/app.css';
+import Routes from './routes';
+
+import { BrowserRouter } from 'react-router-dom';
+
+const App = () =>{
+    return(
+        <BrowserRouter>
+            <Routes/>
+        </BrowserRouter>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root')
+);
+
---
+++ src/routes.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Layout from './hoc/Layout';
+import { Switch, Route } from 'react-router-dom';
+
+const Routes = () =>{
+  return (
+    <Layout>
+      <Switch>
+        
+      </Switch>
+    </Layout>
+  )
+}
+
+export default Routes;
